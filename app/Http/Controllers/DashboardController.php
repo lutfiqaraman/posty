0120b4626cd9fbe3fb778c45f26a3fe8d4e8245b
lutfiqaraman <lutfiqaraman@gmail.com>
2021-01-09 22:22:52 +0200

    authenticate login

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,15 @@ namespace App\Http\Controllers;
 
 class DashboardController extends Controller
 {
+
+    /**
+     * DashboardController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
     public  function Index()
     {
         return view('dashboard.index');
