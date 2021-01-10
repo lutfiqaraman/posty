bffbcdb6f811c632c9dc966a1fcd0262eacb66e8
lutfiqaraman <lutfiqaraman@gmail.com>
2021-01-10 21:42:07 +0200

    Render post page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Auth\RegisterController;
 use \App\Http\Controllers\Auth\LoginController;
@@ -23,3 +24,5 @@ Route::post('/login', [LoginController::class, 'Store']);
 
 Route::get('/register', [RegisterController::class, 'Index'])->name('register');
 Route::post('/register', [RegisterController::class, 'Store']);
+
+Route::get('/posts', [PostController::class, 'Index'])->name('posts');
