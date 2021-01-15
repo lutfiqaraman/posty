42072b7237e2706117801e13770a6cecffbecc75
lutfiqaraman <lutfiqaraman@gmail.com>
2021-01-15 18:59:41 +0200

    use a post component

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,3 +1,29 @@
-<div>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
+@props(['post' => $post])
+
+<div class="mb-4">
+    <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a>
+    <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+
+    <p class="mb-2">{{ $post->body }}</p>
+
+    <div>
+        <form action="{{ route('posts.destroy', $post) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="text-blue-500">Delete this post</button>
+        </form>
+    </div>
+
+    <div class="flex items-center">
+        <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+            @csrf
+            <button type="submit" class="text-blue-500">Like</button>
+        </form>
+        <form action="" method="post" class="mr-1">
+            @csrf
+            <button type="submit" class="text-blue-500">Unlike</button>
+        </form>
+
+        <span>{{ $post->likes->count() }} likes</span>
+    </div>
 </div>
