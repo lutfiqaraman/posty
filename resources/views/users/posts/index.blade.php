307807ae23d28b18538384fd587bc0cfaf5f5b4b
lutfiqaraman <lutfiqaraman@gmail.com>
2021-01-15 19:21:37 +0200

    show posts per each user

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -2,8 +2,25 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-            <p>{{ $user->name }}</p>
+        <div class="w-8/12">
+            <div class="p-6">
+                <h3 class="text-2xl font-medium mb-1">{{ $user->name }}</h3>
+                <p>
+                    Posted {{ $posts->count() }}
+                    {{ \Illuminate\Support\Str::plural('post', $posts->count()) }}
+                </p>
+            </div>
+            <div class="bg-white p-6 rounded-lg">
+                @if($posts->count())
+                    @foreach($posts as $post)
+                        <x-post :post="$post"></x-post>
+                    @endforeach
+
+                    {{ $posts->links() }}
+                @else
+                    <p>{{ $user->name }} does not have any posts</p>
+                @endif
+            </div>
         </div>
     </div>
 @endsection
